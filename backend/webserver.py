7750b91bfa36a1fbccc9cdfd2f2b5a9225ce4a5a
studansp <pstudans@gmail.com>
2015-11-18 14:50:17 -0500

    Updates to client server, get_pending_games now successful

--- backend/webserver.py
+++ backend/webserver.py
@@ -6,6 +6,7 @@ import urlparse
 
 import logic_engine
 import json
+import web_response
 
 # Example python custom handler code courtesy stackoverflow
 # http://stackoverflow.com/questions/4233218/python-basehttprequesthandler-post-variables
@@ -15,6 +16,9 @@ HOST_NAME = '0.0.0.0'
 PORT_NUMBER = 65500
 
 class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    def getSuccessData(self, dataObject):
+        jsonData = web_response.WebResponse(dataObject)
+        return json.dumps(jsonData.__dict__)
     def do_HEAD(self):
         self.send_response(200)
         self.send_header("Content-type", "application/json")
@@ -40,7 +44,10 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         self.send_header("Content-type", "application/json")
         self.end_headers()
 
-        self.wfile.write(json.dumps(response))
+        gamesData = dict()
+        gamesData['games'] = response
+        test = self.getSuccessData(gamesData)
+        self.wfile.write(test)
 
     def do_POST(self):
         length = int(self.headers["Content-Length"])
@@ -88,10 +95,10 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 if __name__ == '__main__':
     server_class = BaseHTTPServer.HTTPServer
     httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
-    print time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER)
+    print(time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER))
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
         pass
     httpd.server_close()
-    print time.asctime(), "Server Stops - %s:%s" % (HOST_NAME, PORT_NUMBER)
+    print(time.asctime(), "Server Stops - %s:%s" % (HOST_NAME, PORT_NUMBER))
